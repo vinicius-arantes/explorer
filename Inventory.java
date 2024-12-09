e77c5b1bad7eef4ac49c5c889a8876a3645be803
Vinicius <vinicius.arantes@compjunior.com.br>
2024-12-09 16:25:39 -0300

    Add basis

---
+++ Inventory.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Inventory here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Inventory extends Actor
+{
+    /**
+     * Act - do whatever the Inventory wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
