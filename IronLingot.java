d336ff565cea325738a175541982f3c832b9f069
mattheus-jordany <mattheusjordany2004@gmail.com>
2024-12-19 15:48:41 -0300

    HUD atualization

---
+++ IronLingot.java
@@ -0,0 +1,32 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class IronLingot here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class IronLingot extends Actor
+{
+    /**
+     * Act - do whatever the IronLingot wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    /**
+     * Act - do whatever the BronzeLingot wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        if (isTouching(Explorer.class)) {
+            // Aumenta a quantidade de Bronze no Inventory
+            Inventory inventory = (Inventory) getWorld().getObjects(Inventory.class).get(0);
+            if (inventory != null) {
+                inventory.takeItenIron();
+            }
+
+            // Remove este objeto do mundo
+            getWorld().removeObject(this);
+        }
+    }
+}
